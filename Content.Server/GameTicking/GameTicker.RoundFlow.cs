774754ec368ef6b5d84cc524dbd6eeb76accebef
CrimeMoot <wakeafa@gmail.com>
2024-10-27 19:00:50 +0500

    fix

--- Content.Server/GameTicking/GameTicker.RoundFlow.cs
+++ Content.Server/GameTicking/GameTicker.RoundFlow.cs
@@ -21,7 +21,6 @@ using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
-using System.Text.RegularExpressions;
 
 namespace Content.Server.GameTicking
 {
@@ -146,6 +145,7 @@ namespace Content.Server.GameTicking
                 LoadGameMap(map, toLoad, null);
             }
         }
+
 
         /// <summary>
         ///     Loads a new map, allowing systems interested in it to handle loading events.
@@ -453,53 +453,9 @@ namespace Content.Server.GameTicking
 
             _replayRoundPlayerInfo = listOfPlayerInfoFinal;
             _replayRoundText = roundEndText;
-            var roundEndSummary = GenerateRoundEndSummary(gamemodeTitle, roundEndText, listOfPlayerInfoFinal);
-            SendRoundEndDiscordMessage(roundEndSummary);
         }
 
-        private string ConvertBBCodeToMarkdown(string text)
-        {
-            text = Regex.Replace(text, @"\[.*?\]", "**");
-
-            return text;
-        }
-
-        private string GenerateRoundEndSummary(string gamemodeTitle, string roundEndText, RoundEndMessageEvent.RoundEndPlayerInfo[] playerInfoArray)
-        {
-            var roundEndTextMarkdown = ConvertBBCodeToMarkdown(roundEndText);
-            var stringBuilder = new System.Text.StringBuilder();
-
-            stringBuilder.AppendLine($"**Режим**: {gamemodeTitle}\n");
-
-            if (!string.IsNullOrWhiteSpace(roundEndTextMarkdown))
-            {
-                stringBuilder.AppendLine($"**Информация**: {roundEndTextMarkdown}\n");
-            }
-
-            var groupedPlayers = playerInfoArray
-                .GroupBy(p => new { p.PlayerOOCName, p.PlayerICName })
-                .Select(g => new
-                {
-                    PlayerOOCName = g.Key.PlayerOOCName,
-                    PlayerICName = g.Key.PlayerICName,
-                    Roles = string.Join(", ", g.Select(p => p.Role).Distinct())
-                })
-                .ToList();
-
-            int totalPlayers = groupedPlayers.Count;
-
-            stringBuilder.AppendLine($"**Всего было игроков**: {totalPlayers}\n");
-            stringBuilder.AppendLine($"**Игроки**:\n");
-
-            foreach (var playerInfo in groupedPlayers)
-            {
-                stringBuilder.AppendLine($"*{playerInfo.PlayerOOCName}* '**{playerInfo.PlayerICName}**' в роли: {playerInfo.Roles}");
-            }
-
-            return stringBuilder.ToString();
-        }
-
-        private async void SendRoundEndDiscordMessage(string roundEndSummary)
+        private async void SendRoundEndDiscordMessage()
         {
             try
             {
@@ -507,10 +463,13 @@ namespace Content.Server.GameTicking
                     return;
 
                 var duration = RoundDuration();
-                var content = $"**Раунд {RoundId} завершен!**\n" +
-                              $"**Продолжительность**: {Math.Truncate(duration.TotalHours)} часов {duration.Minutes} минут {duration.Seconds} секунд\n" +
-                              $"{roundEndSummary}";
+                var content = Loc.GetString("discord-round-notifications-end",
+                    ("id", RoundId),
+                    ("hours", Math.Truncate(duration.TotalHours)),
+                    ("minutes", duration.Minutes),
+                    ("seconds", duration.Seconds));
                 var payload = new WebhookPayload { Content = content };
+
                 await _discord.CreateMessage(_webhookIdentifier.Value, payload);
 
                 if (DiscordRoundEndRole == null)
