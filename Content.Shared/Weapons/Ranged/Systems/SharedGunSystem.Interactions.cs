f7d1db2ab5d2f7b53d56be831b71e98bf41cce30
CrimeMoot <wakeafa@gmail.com>
2024-11-09 03:28:02 +0500

    fix

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Interactions.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Interactions.cs
@@ -63,24 +63,10 @@ public abstract partial class SharedGunSystem
 
     private void SelectFire(EntityUid uid, GunComponent component, SelectiveFire fire, EntityUid? user = null)
     {
-        if (component.GunOwner != null)
-        {
-            if (component.GunOwner != user)
-            {
-                Popup(Loc.GetString("gun-already-personalized", ("owner", component.GunOwner)), uid, user);
-                return;
-            }
-        }
-        else if (component.Personable && user != null)
-        {
-            Popup(Loc.GetString("gun-unauthorized-user"), uid, user);
-            return;
-        }
-
         if (component.SelectedMode == fire)
             return;
 
-        DebugTools.Assert((component.AvailableModes & fire) != 0x0);
+        DebugTools.Assert((component.AvailableModes  & fire) != 0x0);
         component.SelectedMode = fire;
 
         if (!Paused(uid))
