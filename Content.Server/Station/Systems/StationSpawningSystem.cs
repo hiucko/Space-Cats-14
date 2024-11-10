1aa37a2d08f0b0037cb42ab5b242ac8469a227bc
CrimeMoot <wakeafa@gmail.com>
2024-11-10 14:57:59 +0500

    radio color

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -49,6 +49,7 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
     [Dependency] private readonly PdaSystem _pdaSystem = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedJobSystem _jobSystem = default!;
 
     private bool _randomizeCharacters;
 
@@ -230,6 +231,13 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
 
         _cardSystem.TryChangeFullName(cardId, characterName, card);
         _cardSystem.TryChangeJobTitle(cardId, jobPrototype.LocalizedName, card);
+
+        var color = jobPrototype.Color is not null ? jobPrototype.Color
+					: _jobSystem.TryGetDepartment(jobPrototype.ID, out var department) ? department.Color 
+					: null;
+
+		if (color is not null)
+			_cardSystem.TryChangeColor(cardId, color);
 
         if (_prototypeManager.TryIndex(jobPrototype.Icon, out var jobIcon))
             _cardSystem.TryChangeJobIcon(cardId, jobIcon, card);
